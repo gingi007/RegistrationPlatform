a437d4ae9f327e5b36948fb74928470cf31ba6d7
Shilo Mangam <shilo.mangam@sap.com>
2017-01-21 12:22:05 +0200

    Enviroment variables sets

--- config/env/development.js
+++ config/env/development.js
@@ -4,23 +4,23 @@ var mongo = process.env.MONGO_URL || '';
 var emailAddr = process.env.EMAIL_ADDR || '';
 var emailPass = process.env.EMAIL_PASS || '';
 var supportEmailAddr = process.env.SUPP_EMAIL_ADDR || '';
+var adminEmail = process.env.ADMIN_EMAIL || '';
 var eventname = 'MTA Hack';
 var eventMediaLinks = {
-	website: 'http://www.mtahack.com',
-	facebook: 'https://www.facebook.com/mtahack2017',
-	twitter: 'https://twitter.com/mtahackathon',
-	google: 'https://plus.google.com/u/1/113260219787307001341/'
+	website: '',
+	facebook: '',
+	twitter: '',
+	google: ''
 };
 var eventfacebook = '';
 var maxNumOfUsersInTeam = 4;
-var adminEmail = 'mta.hackathon@gmail.com';
 module.exports = {
 	port: port,
 	host: host,
-	db: 'mongodb://localhost/registrationplatform',
-	emailAddr: 'mta.hackathon@gmail.com',
-	emailPass: 'mtahack2017',
-	supportEmailAddr: 'mta.hackathon@gmail.com',
+	db: mongo,
+	emailAddr: emailAddr,
+	emailPass: emailPass,
+	supportEmailAddr: supportEmailAddr,
 	eventname: eventname,
 	eventwebsite: eventMediaLinks.website,
 	eventMediaLinks: eventMediaLinks,
